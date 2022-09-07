284f8ee924cad4a9bd0cf3b1649b3801f43a9a97
Kevin Terrier <kevin.terrier@le1817.com>
2022-09-07 13:51:59 +0200

    added description

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import spaceBg from '../assets/space-bg.jpg'
+import card1 from '../assets/card1.jpg'
 
 const Home = () => {
   return (
@@ -14,11 +15,17 @@ const Home = () => {
           <p className="absolute top-2/4 text-white text-center text-3xl w-full ">
             Get Nasa Astronomy informations
           </p>
-          <div className="absolute top-3/4  w-full">
-            <div className=" text-white text-center text-3xl w-10 mx-45  mx-auto h-24  ">
+          <div className="absolute top-2/4  w-full">
+            <div className=" text-white text-center text-3xl w-10 mx-45  mx-auto h-48 ">
               <div className="arrow arrow-first"></div>
               <div className="arrow arrow-second"></div>
             </div>
+          </div>
+        </div>
+        <div className="w-full bg-white pt-8">
+          <div className="relative flex  bg-white w-1/2 mx-auto justify-between items-center  ">
+            <img src={card1} className="h-96  rounded-lg " />
+            <p className="text">Find datas for many planets.</p>
           </div>
         </div>
       </div>
