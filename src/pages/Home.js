fecacf32a7049e7b7e24ca8892fcff3d0a5f1bcf
Kevin Terrier <kevin.terrier@le1817.com>
2022-09-07 11:52:41 +0200

    fixed image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,13 +4,13 @@ const Home = () => {
   return (
     <div>
       <div className="w-screen h-screen">
-        <div className="w-screen">
+        <div className=" relative">
           <img
             src={spaceBg}
             alt="Nasapi background showing  up space"
-            className=" object-fill "
+            className=" object-cover max-h-screen w-full	 "
           />
-          <div className=""></div>
+          <div className="bg-dark-blue w-screen h-screen absolute top-0 opacity-40	"></div>
         </div>
       </div>
     </div>
